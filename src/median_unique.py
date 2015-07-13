83873ff9b2dc501e813a4c782b94ca51ba8920de
Sergey <SergeyKstnko@users.noreply.github.com>
2015-07-13 07:38:54 -0400

    The new version works 11 times faster
    Thank you for allowing to update the file.
    
    The new version works 11 times faster. Decrease from 11.873 to 0.225 seconds.
    Problem: numpy.median() function was sorting array for every tweet.
    Solution: Bisect.insort_left() allowed me to insert number of words in the tweet into the array in the sorted fashion. It saved me 3 seconds to run the code (from 11.8 to 9.3 seconds). But even then numpy.median() didn't know that array is sorted, therefore it sorted the array using quicksort faster, but still spent time on it. Therefore I implemented my own median function, that skips sorting and finds median for the array.

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,25 +1,36 @@
 #median_unique.py
-#Calculate the median number of unique words per tweet, 
-#and update this median as tweets come in.
 
 # author Sergey Kostenko
 
+#Calculate the median number of unique words per tweet, 
+#and update this median as tweets come in.
 
-import numpy
+import bisect
 
 #paths to files
 read_path = '../tweet_input/tweets.txt'
 write_path = "../tweet_output/ft2.txt"
+    
 
+#This function calculates median for the file. The reason I wrote my own function
+#vs using numpy.median() is that numpy.median() sorts array even in case if it is
+#already sorted. And it take a lot of time.
+def median(array):
+    if len(array)%2 !=0:
+        return array[len(array)/2]
+    return (array[len(array)/2-1] + array[len(array)/2])/2.0
+            
 try:
     with open(read_path) as inf, open(write_path, 'a') as outf:
         array = []
         #for every line (tweet) in the file
         for line in inf:
-            #append amount of unique words to the array
-            array.append(len(set(line.split())))
-            #write current median of the array to the file
-            result = "{:.2f}\n".format(numpy.median(array))
+            #find amount of unique words in the tweet
+            k = len(set(line.split()))
+            #insert amount of unique words into the array in the sorted fashion
+            bisect.insort_left(array, k)
+            #write current median of the array into the file
+            result = "{:.2f}\n".format(median(array))
             outf.write(result)
 except IOError as e:
     print 'Operation failed: %s' % e.strerror
